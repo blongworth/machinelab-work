be3ace032dab1bb77db1d6139a2cfc2315961431
Brett Longworth <blongworth@whoi.edu>
2023-05-16 15:13:16 -0400

    add tests

--- find-missing-adv.py
+++ find-missing-adv.py
@@ -2,11 +2,6 @@
 
 import re
 
-test = [*range(0,256)]
-testgood = test + test
-
-testbad = testgood[:1] + testgood[2:]
-
 # find missing numbers in an array
 def findMissingNumbers(numlist):
     misscount = 0
@@ -20,21 +15,36 @@ def findMissingNumbers(numlist):
 def missFrac(numlist):
     return findMissingNumbers(numlist) / len(numlist)
 
-findMissingNumbers(testgood)
-findMissingNumbers(testbad)
-
-missFrac(testgood)
-missFrac(testbad)
-
-
+def readSequence(data_file):
+    with open(data_file) as f:
+        sequence = []
+        for line in f:
+            if line.startswith("D:"):
                 sequence.append(int(re.search(r'\d+', line).group()))
     return sequence
 
+### Tests
+# Test data
+test = [*range(0,256)]
+testgood = test + test
+testbad = testgood[:1] + testgood[2:]
+testbad2 = testgood[:1] + testgood[3:]
+
+# tests
+findMissingNumbers(testgood) #expect 0
+findMissingNumbers(testbad)  #expect 1
+findMissingNumbers(testbad2) #expect 2
+
+missFrac(testgood) # expect 0
+missFrac(testbad)  # expect ~0.002
+
+# check on lander data
 file = '2023_05_05_13_13_09.txt'
 fileseq = readSequence(file)
 findMissingNumbers(fileseq)
 missFrac(fileseq)
 
+# check on surface data
 file = '2023_05_05_12_13_22_sfc.txt'
 fileseq = readSequence(file)
 findMissingNumbers(fileseq)
