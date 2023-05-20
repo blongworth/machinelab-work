d3228f0b09e4ecad50edcdfd5fd41c97088df6df
Brett Longworth <blongworth@whoi.edu>
2023-05-20 11:07:21 -0400

    add SD data parser

---
+++ parse_LECS_data.R
@@ -0,0 +1,36 @@
+#!/usr/bin/R
+# concatenate LECS SD data files
+
+library(tidyverse)
+library(mlabtools)
+
+# Lander data
+# list files
+file_dir <- "data/LECS_2023/LECS_Lander"
+files <- list.files(file_dir, pattern = "^2023", full.names = TRUE)
+
+status <- parse_status(files)
+write_csv(status, "data/LECS_2023/lander_status.csv")
+
+# maybe need to do data in chunks?
+adv_data <- parse_data(files[541])
+write_csv(adv_data, "data/LECS_2023/lander_data.csv")
+
+
+# Surface SD data
+# list files
+file_dir <- "data/LECS_2023/LECS_Surface/"
+files <- list.files(file_dir, pattern = "^2023", full.names = TRUE)
+
+# status
+status <- parse_status(files)
+write_csv(status, "data/LECS_2023/surface_status.csv")
+
+# ADV data
+# maybe need to do data in chunks?
+adv_data <- parse_data(files[541])
+write_csv(adv_data, "data/LECS_2023/surface_data.csv")
+
+# met data
+met <- parse_met(files)
+write_csv(status, "data/LECS_2023/surface_met.csv")
