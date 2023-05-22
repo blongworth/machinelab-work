3a41257596f50dcb2353ade178893b984e2acbc9
Brett Longworth <blongworth@whoi.edu>
2023-05-22 07:38:27 -0400

    fix paths

--- parse_LECS_data.R
+++ parse_LECS_data.R
@@ -6,7 +6,7 @@ library(mlabtools)
 
 # Lander data
 # list files
-file_dir <- "data/LECS_2023/LECS_Lander"
+file_dir <- "data/LECS_2023/LECS_Lander/SDHC/"
 files <- list.files(file_dir, pattern = "^2023", full.names = TRUE)
 
 status <- parse_status(files)
@@ -19,7 +19,7 @@ write_csv(adv_data, "data/LECS_2023/lander_data.csv")
 
 # Surface SD data
 # list files
-file_dir <- "data/LECS_2023/LECS_Surface/"
+file_dir <- "data/LECS_2023/LECS_Surface/SDHC/"
 files <- list.files(file_dir, pattern = "^2023", full.names = TRUE)
 
 # status
@@ -28,9 +28,9 @@ write_csv(status, "data/LECS_2023/surface_status.csv")
 
 # ADV data
 # maybe need to do data in chunks?
-adv_data <- parse_data(files[541])
+adv_data <- parse_data(files[130])
 write_csv(adv_data, "data/LECS_2023/surface_data.csv")
 
 # met data
 met <- parse_met(files)
-write_csv(status, "data/LECS_2023/surface_met.csv")
+write_csv(met, "data/LECS_2023/surface_met.csv")
