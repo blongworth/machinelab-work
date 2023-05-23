f370dbda9664ed7a0a398b4bd74fa01592dcd8a2
Brett Longworth <blongworth@whoi.edu>
2023-05-23 07:12:38 -0400

    fix wrap count

--- find-missing-adv.py
+++ find-missing-adv.py
@@ -6,8 +6,8 @@ import re
 def findMissingNumbers(numlist):
     misscount = 0
     for i in range( 1, len(numlist)):
-        if numlist[i] == 0:
-            misscount += 255 - numlist[i-1]
+        if numlist[i] < numlist[i-1]:
+            misscount += 255 + numlist[i] - numlist[i-1]
         else:
             misscount += numlist[i] - 1 - numlist[i-1]
     return misscount
@@ -29,11 +29,16 @@ test = [*range(0,256)]
 testgood = test + test
 testbad = testgood[:1] + testgood[2:]
 testbad2 = testgood[:1] + testgood[3:]
+testbad3 = test + test[1:]
+testbad4 = test + test[2:]
+
 
 # tests
 findMissingNumbers(testgood) #expect 0
 findMissingNumbers(testbad)  #expect 1
 findMissingNumbers(testbad2) #expect 2
+findMissingNumbers(testbad3) #expect 1
+findMissingNumbers(testbad4) #expect 1
 
 missFrac(testgood) # expect 0
 missFrac(testbad)  # expect ~0.002
