99489c50ef3ccfcfd05d06ea585ab835aebe9f5d
Brett Longworth <blongworth@whoi.edu>
2024-01-16 08:29:29 -0500

    add web publish script

---
+++ publish_lecs.sh
@@ -0,0 +1,7 @@
+# render and publish LECS data from website
+# publish to https://blongworth.github.io/machinelab-work/
+
+quarto publish read_parse_web.qmd
+# doesn't work
+# quarto publish read_parse_web.qmd -P start_date:2024-01-01
+
