c933a7ba6341770643588ed32c24c9d2832bc54b
Brett Longworth <blongworth@whoi.edu>
2024-01-17 14:11:52 -0500

    Update to work from cron

--- publish_lecs.sh
+++ publish_lecs.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 # render and publish LECS data from website
+# use data from the last week
 # publish to https://blongworth.github.io/machinelab-work/
 
-quarto publish read_parse_web.qmd
-# doesn't work
-# quarto publish read_parse_web.qmd -P start_date:2024-01-01
+quarto render read_parse_web.qmd -P start_date:$(date -v-7d "+%Y-%m-%d")
+quarto publish read_parse_web.qmd --no-render --no-prompt --no-browser
 
