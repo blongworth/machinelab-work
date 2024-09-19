b4c6d854fc5c69f2e541bd65a62b1d86fbe23dfe
Brett Longworth <blongworth@whoi.edu>
2024-09-19 16:50:17 -0400

    add august data, add all seaphox

--- lecs_process_all.R
+++ lecs_process_all.R
@@ -13,9 +13,7 @@ lecs_process_data(date = "2024-01-23", file_dir = "/Users/brett/Projects/machine
 lecs_process_data(date = "2024-02-28", file_dir = "/Users/brett/Projects/machinelab-work/data/SD Card Data/LECS_surface_sd/lecs_surface_2024-02-28/clean", out_dir = out_dir, dedupe = FALSE)
 lecs_process_data(date = "2024-05-14", file_dir = "/Users/brett/Projects/machinelab-work/data/SD Card Data/LECS_surface_sd/lecs_surface_2024-05-14/clean", out_dir = out_dir, dedupe = FALSE)
 lecs_process_data(date = "2024-06-21", file_dir = "/Users/brett/Projects/machinelab-work/data/SD Card Data/LECS_surface_sd/lecs_surface_2024-06-21", out_dir = out_dir, dedupe = FALSE)
-#lecs_process_data("2023-09-05", out_dir = out_dir, dedupe = TRUE)
-#lecs_process_data("2023-11-15", out_dir = out_dir, dedupe = FALSE)
-#lecs_process_data("2024-01-23", out_dir = out_dir, dedupe = FALSE)
+lecs_process_data(date = "2024-08-27", file_dir = "/Users/brett/Projects/machinelab-work/data/SD Card Data/LECS_surface_sd/lecs_surface_2024-08-27", out_dir = out_dir, dedupe = FALSE)
 toc()
 
 #out_dir <- "data/processed/lander/"
